e7f6e4ff0b184fb0877d33d83b1526d33ec5818c
Duy <duyvub1206@gmail.com>
2024-10-18 16:12:03 +0700

    test

--- lib/fe/screen/HomeScreen.dart
+++ lib/fe/screen/HomeScreen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: const Text('Home Screen'),
         backgroundColor: Colors.blue,
       ),
       body: Center(
