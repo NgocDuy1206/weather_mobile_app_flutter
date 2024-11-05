7963b93b16eb72ae308db3879d96e1fa77da6596
vamostmc <trinhminhchienx@gmail.com>
2024-11-05 18:41:25 +0700

    add screen add_widget

--- lib/fe/screen/Screen1.dart
+++ lib/fe/screen/Screen1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'display_search_manageLocation/add_widget_screen.dart';
+import 'display_search_manageLocation/manage_location_screen.dart';
 import 'display_search_manageLocation/search_screen.dart';
 
 class Screen1 extends StatelessWidget {
@@ -8,22 +10,44 @@ class Screen1 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Screen 1')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              isScrollControlled: true,
-              backgroundColor: Color(0xFF1A0A1A),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-              ),
-              builder: (BuildContext context) {
-                //Nhảy sang search_screen
-                return SearchScreenModal();
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  backgroundColor: Color(0xFF1A0A1A),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  builder: (BuildContext context) {
+                    return SearchScreenModal();
+                  },
+                );
               },
-            );
-          },
-          child: Text('Mở Tìm Kiếm'),
+              child: Text('Search'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ManageScreen()),
+                );
+              },
+              child: Text('Manage'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddWidgetScreen()),
+                );
+              },
+              child: Text('Add Widget'),
+            ),
+          ],
         ),
       ),
     );
